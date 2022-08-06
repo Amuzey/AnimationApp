0d13733cf48ac2d8d3f467965c6713510904dcbf
Amuzey <ivanovad97@gmail.com>
2022-08-06 12:19:42 +0500

    work with logic

--- AnimationApp/ViewControllers/AnimationViewController.swift
+++ AnimationApp/ViewControllers/AnimationViewController.swift
@@ -30,6 +30,28 @@ class AnimationViewController: UIViewController {
 
     @IBAction func animationButtonPresed() {
         
+        let randomAnimation = animations.randomElement()
+        
+        guard let preset = randomAnimation?.preset else { return }
+        guard let curve = randomAnimation?.curve else { return }
+        guard let force = randomAnimation?.force else { return }
+        guard let delay = randomAnimation?.delay else { return }
+        guard let duration = randomAnimation?.duration else { return }
+        springAnimationView.animation = preset
+        springAnimationView.delay = delay
+        springAnimationView.force = force
+        springAnimationView.duration = duration
+        springAnimationView.curve = curve
+        animationLabel.text = """
+        preset: \(preset)
+        curve: \(curve)
+        force: \(String(format: "%.2F", force))
+        delay: \(String(format: "%.2F", delay))
+        duration: \(String(format: "%.2F", duration))
+        """
+        springAnimationView.animate()
+        }
+       
     }
-}
+
 
